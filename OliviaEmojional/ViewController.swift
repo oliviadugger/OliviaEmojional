f11dcce3b9e7ce009a32f38d210bf3bc6e10ccfc
Olivia Dugger <oliviadugger@Olivias-MacBook-Air.local>
2019-06-20 16:15:46 -0500

    Iteration 3 Part

--- OliviaEmojional/ViewController.swift
+++ OliviaEmojional/ViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let weathers = ["☀️": "sunny", "🌧": "stormy", "❄️": "snowy", "🌪": "tornado"]
+
+    let customMessages = ["sunny":["Wear sunscreen!", "Walkin on sunshine!"], "stormy": ["Stay indoors!", "Watch out for lightning!"], "snowy":["Make a snow angel!", "Wear lots of layers!"], "tornado": ["We're not in Kansas anymore!", "Stay safe!"]]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     @IBAction func showMessage(sender: UIButton) {
-       let alertController = UIAlertController(title: "WEATHER ALERT", message: "...", preferredStyle: UIAlertController.Style.alert)
+        let selectedWeather = sender.titleLabel!.text!
+        let emojiMessage = customMessages[weathers[selectedWeather]!]?[0]
+
+        
+        
+        let alertController = UIAlertController(title: "WEATHER ALERT", message:  weathers[emojiMessage!], preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)

4b18fda03344f465d0eed69f4c1012198dec3b3e
Olivia Dugger <oliviadugger@Olivias-MacBook-Air.local>
2019-06-20 15:03:10 -0500

    Iteration 0

---
+++ OliviaEmojional/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  OliviaEmojional
+//
+//  Created by Olivia Dugger on 6/20/19.
+//  Copyright © 2019 Olivia Dugger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        
+    }
+
+
+}
+

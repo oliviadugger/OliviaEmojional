acd473fc3f8b8d1ceb7a1f3d7c9c9a4df53f493a
Olivia Dugger <oliviadugger@Olivias-MacBook-Air.local>
2019-06-21 09:39:40 -0500

    Finished Project

--- OliviaEmojional/ViewController.swift
+++ OliviaEmojional/ViewController.swift
@@ -12,19 +12,20 @@ class ViewController: UIViewController {
 
     let weathers = ["☀️": "sunny", "🌧": "stormy", "❄️": "snowy", "🌪": "tornado"]
 
-    let customMessages = ["sunny":["Wear sunscreen!", "Walkin on sunshine!"], "stormy": ["Stay indoors!", "Watch out for lightning!"], "snowy":["Make a snow angel!", "Wear lots of layers!"], "tornado": ["We're not in Kansas anymore!", "Stay safe!"]]
+    let customMessages = ["sunny":["Make sure to wear sunscreen!", "I'm walkin on sunshine!", "Time for a swim!"], "stormy": ["Stay indoors!", "It's raining men, HALLELUJAH!", "I'm siiiiinging in the rain!"], "snowy":["Make a snow angel!", "Make sure to wear lots of layers!"], "tornado": ["We're not in Kansas anymore!", "Stay safe!", "Duck for cover!"]]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     @IBAction func showMessage(sender: UIButton) {
+        let num = Int.random(in: 0...2)
         let selectedWeather = sender.titleLabel!.text!
-        let emojiMessage = customMessages[weathers[selectedWeather]!]?[0]
+        let emojiMessage = customMessages[weathers[selectedWeather]!]?[num]
 
         
         
-        let alertController = UIAlertController(title: "WEATHER ALERT", message:  weathers[emojiMessage!], preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: "WEATHER ALERT", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
